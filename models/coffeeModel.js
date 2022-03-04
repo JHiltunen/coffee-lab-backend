4b65a0757d67443851c40d060921c7598d7ceece
Joonas Hiltunen <joonas.s.jh@gmail.com>
2022-03-04 21:59:35 +0200

    Add controller and model

---
+++ models/coffeeModel.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const getAllCoffees = () => {
+
+};
+
+const addCoffee = (coffee) => {
+
+};
+
+module.exports = {
+    getAllCoffees,
+    addCoffee
+}
--- routes/coffeeRouter.js
+++ routes/coffeeRouter.js
@@ -1,8 +1,12 @@
+'use strict';
+
 const express = require('express');
 const router = express.Router();
+const coffeeController = require('../controllers/coffeeController')
 
 router.route('/coffees')
     .get()
     .post()
 
 router.route('/coffees/search')
+    .get()
