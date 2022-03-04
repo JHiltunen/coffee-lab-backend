c271e87596126943dfa9309980757a978c5b9850
Joonas Hiltunen <joonas.s.jh@gmail.com>
2022-03-04 21:47:23 +0200

    Added basic project structure

---
+++ app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public')); // serve static content of public folder. These files can be accessed publicly
+app.use(express.json()); // for parsing application/json
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
