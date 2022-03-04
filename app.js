9d50093651c671832051c1832d9356934fa2d995
Joonas Hiltunen <joonas.s.jh@gmail.com>
2022-03-04 21:49:07 +0200

    Add router

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const coffeeRouter = require('./routes/coffeeRouter')
 
 app.use(express.static('public')); // serve static content of public folder. These files can be accessed publicly
 app.use(express.json()); // for parsing application/json
