88d6cff6428bc4ac64b3da2195049d8b99922b0d
Joonas Hiltunen <joonas.s.jh@gmail.com>
2022-03-06 15:09:47 +0200

    Change port number

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 8080
 const coffeeRouter = require('./routes/coffeeRouter');
 
 app.use(express.static('public')); // serve static content of public folder. These files can be accessed publicly
