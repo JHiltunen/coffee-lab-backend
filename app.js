f70688b54a51a87898c12936ca5bcb2c990edb79
Joonas Hiltunen <joonas.s.jh@gmail.com>
2022-03-06 15:24:55 +0200

    Added Cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 'use strict';
 
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 8080
 const coffeeRouter = require('./routes/coffeeRouter');
 
+app.use(cors());
 app.use(express.static('public')); // serve static content of public folder. These files can be accessed publicly
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
